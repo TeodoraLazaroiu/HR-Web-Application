d36dee30b56a59211d57708e232055237b1acf00
TeodoraLazaroiu <lazaroiuteodora@yahoo.com>
2023-05-04 23:10:05 +0300

    Added authentication service test class

---
+++ UnitTests/AuthenticationServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using WebAPI.Repository.Interfaces;
+using WebAPI.Services;
+
+namespace UnitTests
+{
+    public class AuthenticationServiceTests
+    {
+        private IAuthenticationService _authenticationService;
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+        private Mock<IConfiguration> _configurationMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _configurationMock = new Mock<IConfiguration>();
+            _authenticationService = new AuthenticationService(_unitOfWorkMock.Object, _configurationMock.Object);
+        }
+
+
+    }
+}
